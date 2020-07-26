ac0f777d8237cf0046ff2a3b0deaf92074a5c19e
Rahul Prajapati <rohngonnarock@gmail.com>
2020-07-26 13:35:49 +0530

    Updated Image path

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Card = ({ article }) => {
+  const imagePath = article.image[0].url;
   const imageUrl =
     process.env.NODE_ENV !== "development"
-      ? article.image.url
-      : process.env.REACT_APP_BACKEND_URL + article.image[0].url;
+      ? imagePath
+      : process.env.REACT_APP_BACKEND_URL + imagePath;
   return (
     <Link to={`/article/${article.id}`} className="uk-link-reset">
       <div className="uk-card uk-card-muted">
